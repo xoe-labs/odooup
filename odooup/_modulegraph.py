5cf008a08a68c3ae75a8434c7d1f602ef20b7750
David Arnold <dar@xoe.solutions>
2019-07-03 16:34:50 +0200

    [IMP] teach white list reading from git index
    when reading from index, we ignore the currently checked out state.
    This makes whit listing work properly even on a scoped checkout

--- odooup/_modulegraph.py
+++ odooup/_modulegraph.py
@@ -7,47 +7,50 @@ import os
 
 import networkx as nx
 
-
-class ManifestContainer(object):
-    pass
-
+from ._helpers import call_cmd
 
 MANIFEST_NAMES = ("__manifest__.py", "__openerp__.py", "__terp__.py")
 SKIP_PATHS = ["point_of_sale/tools"]
 
 
-class NoManifestFound(Exception):
-    pass
-
-
-def _get_manifest_path(addon_dir):
-    for manifest_name in MANIFEST_NAMES:
-        manifest_path = os.path.join(addon_dir, manifest_name)
-        if os.path.isfile(manifest_path):
-            return manifest_path
-
-
-def _parse_manifest(s):
+def _parse_manifest_from_git(repo_path, manifest_object):
+    s = call_cmd(
+        "git cat-file -p {manifest_object}".format(**locals()),
+        echo_cmd=False,
+        exit_on_error=False,
+        cwd=repo_path,
+    )
     return ast.literal_eval(s)
 
 
-def _read_manifest(addon_dir):
-    manifest_path = _get_manifest_path(addon_dir)
-    if not manifest_path:
-        raise NoManifestFound("no Odoo manifest found in %s" % addon_dir)
-    with open(manifest_path) as mf:
-        return _parse_manifest(mf.read())
-
-
 def _find_addons(dir):
-    """ yield (addon_name, addon_dir, manifest) """
-    for root, _, files in os.walk(dir):
-        if ".git" in root:
-            continue
-        if any(s in root for s in SKIP_PATHS):
-            continue
-        if any(M in files for M in MANIFEST_NAMES):
-            yield os.path.dirname(root), os.path.basename(root), _read_manifest(root)
+    """ yield (addon_dir, addon_name, manifest) """
+    project = call_cmd(
+        "git ls-tree -r HEAD", echo_cmd=False, exit_on_error=False, cwd=dir
+    ).split("\n")
+    project = [i.split() for i in project]
+    manifests = {
+        (".", i[2], i[3])
+        for i in project
+        if i[1] == "blob"
+        and any(M in i[3] for M in MANIFEST_NAMES)
+        and not any(S in i[3] for S in SKIP_PATHS)
+    }
+    project_submodule_paths = [i[3] for i in project if i[1] == "commit"]
+    for path in project_submodule_paths:
+        submodule = call_cmd(
+            "git ls-tree -r HEAD", echo_cmd=False, exit_on_error=False, cwd=path
+        ).split("\n")
+        submodule = [i.split() for i in submodule]
+        manifests |= {
+            (path, i[2], os.path.join(path, i[3]))
+            for i in submodule
+            if i[1] == "blob" and any(M in i[3] for M in MANIFEST_NAMES)
+        }
+    for repo_path, manifest_object, manifest_path in manifests:
+        module_path = os.path.dirname(manifest_path)
+        manifest = _parse_manifest_from_git(repo_path, manifest_object)
+        yield os.path.dirname(module_path), os.path.basename(module_path), manifest
 
 
 def get_graph(dir):
