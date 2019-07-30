5744743982fedc5a754a8a23c54ce55e51b1bf13
David Arnold <dar@xoe.solutions>
2019-07-31 01:21:22 +0200

    [IMP] check packege versions during install
    Very naive implementation, but better than othing. Use on debian.
    Else, you probably know what you're doing anyhow :D

--- odooup/_installers.py
+++ odooup/_installers.py
@@ -8,12 +8,64 @@ import click
 from ._helpers import call_cmd
 
 
+def check_versions():
+    try:
+        git_ok = (
+            call_cmd("git version", exit_on_error=False, echo_cmd=False)
+            >= "git version 2.22.0"
+        )
+    except Exception:
+        git_ok = False
+    if not git_ok:
+        click.get_current_context().fail(
+            "Please install git version > 2.22.0.\n"
+            "In debian, you can do:\n"
+            "\t$ sudo add-apt-repository ppa:git-core/ppa -y\n"
+            "\t$ sudo apt-get update\n"
+            "\t$ sudo apt-get install --upgrade git -y\n"
+        )
+    try:
+        docker_ok = (
+            call_cmd("docker --version", exit_on_error=False, echo_cmd=False).split(
+                ","
+            )[0]
+            >= "Docker version 18.09.6"
+        )
+    except Exception:
+        docker_ok = False
+    if not docker_ok:
+        click.get_current_context().fail(
+            "Please install docker version >= 18.09.6.\n"
+            "Follow instructions from \n"
+            "\thttps://docs.docker.com/install/linux/docker-ce/ubuntu/"
+        )
+    try:
+        docker_compose_ok = (
+            call_cmd(
+                "docker-compose --version", exit_on_error=False, echo_cmd=False
+            ).split(",")[0]
+            >= "docker-compose version 1.21.0"
+        )
+    except Exception:
+        docker_compose_ok = False
+    if not docker_compose_ok:
+        click.get_current_context().fail(
+            "Please install docker-compose version >= 1.21.0.\n"
+            "Follow instructions from \n"
+            "\thttps://docs.docker.com/compose/install/"
+        )
+
+
 def install_make():
     click.secho("We need to install make ...\n", fg="red")
     if platform.system() != "Windows":
+        if not find_executable("apt"):
+            click.get_current_context().fail(
+                "Can't find apt package manager. Please install make manually."
+            )
         call_cmd("sudo -k -H apt install make")
     else:
-        click.fail(
+        click.get_current_context().fail(
             "As a windows user, you must install `make` manually. "
             "See: http://bfy.tw/NI8r"
         )
@@ -26,6 +78,10 @@ def install_precommit():
         fg="red",
     )
     if platform.system() != "Windows":
+        if not find_executable("pip"):
+            click.get_current_context().fail(
+                "Can't find pip python package manager. Please install pip manually."
+            )
         call_cmd("sudo -k -H pip install pre-commit")
     else:
         call_cmd("pip install pre-commit")
@@ -61,6 +117,7 @@ def install_compose_impersonation():
 
 
 def install_tools():
+    check_versions()
     # Make sure basic stuff is there
     if not find_executable("make"):
         install_make()
