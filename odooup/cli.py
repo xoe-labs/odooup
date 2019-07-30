7bb8b526f9b6ea881798d5924c62ff9dfa21f241
David Arnold <dar@xoe.solutions>
2019-07-31 01:47:15 +0200

    [REL} release with better semantics

--- odooup/cli.py
+++ odooup/cli.py
@@ -2,7 +2,7 @@
 # Copyright 2019 XOE Labs (<https://xoe.solutions>)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-__version__ = "0.0.22"
+__version__ = "0.0.23"
 
 import sys
 
