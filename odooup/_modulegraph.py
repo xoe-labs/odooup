187627fe8ad12a5129781d5949cdd0386fd9dca0
David Arnold <dar@xoe.solutions>
2019-09-24 15:01:39 -0500

    [FIX] graph loading order as per DockeryOdoo loading order
    Module drop-in overrides can redefine a module, therefore it is
    important to build the graph after the same loading order as DockeryOdoo

--- odooup/_modulegraph.py
+++ odooup/_modulegraph.py
@@ -29,25 +29,50 @@ def _find_addons(dir):
         "git ls-tree -r HEAD", echo_cmd=False, exit_on_error=False, cwd=dir
     ).split("\n")
     project = [i.split() for i in project]
-    manifests = {
-        (".", i[2], i[3])
-        for i in project
-        if i[1] == "blob"
-        and any(M in i[3] for M in MANIFEST_NAMES)
-        and not any(S in i[3] for S in SKIP_PATHS)
-    }
     project_submodule_paths = [i[3] for i in project if i[1] == "commit"]
-    for path in project_submodule_paths:
+    manifests = {}
+
+    def _get_module_name(manifest_path):
+        return os.path.basename(os.path.dirname(manifest_path))
+
+    # First iterate on submodules in reversed alphabetical
+    # order (same as DockeryOdoo) -- for drop in module overrides
+    for submodule_path in reversed(sorted(project_submodule_paths)):
         submodule = call_cmd(
-            "git ls-tree -r HEAD", echo_cmd=False, exit_on_error=False, cwd=path
+            "git ls-tree -r HEAD",
+            echo_cmd=False,
+            exit_on_error=False,
+            cwd=submodule_path,
         ).split("\n")
         submodule = [i.split() for i in submodule]
-        manifests |= {
-            (path, i[2], os.path.join(path, i[3]))
-            for i in submodule
-            if i[1] == "blob" and any(M in i[3] for M in MANIFEST_NAMES)
+        manifests.update(
+            {
+                _get_module_name(i[3]): (  # module name
+                    submodule_path,  # submodule base path
+                    i[2],  # manifest object
+                    os.path.join(submodule_path, i[3]),  # full manifest path
+                )
+                for i in submodule
+                if i[1] == "blob"
+                and any(M in i[3] for M in MANIFEST_NAMES)
+                and not any(S in i[3] for S in SKIP_PATHS)
+            }
+        )
+    # Always give src highest priority
+    manifests.update(
+        {
+            _get_module_name(i[3]): (  # module name
+                ".",  # relative to root repo path
+                i[2],  # manifest object
+                i[3],  # full manifest path
+            )
+            for i in project
+            if i[1] == "blob"
+            and any(M in i[3] for M in MANIFEST_NAMES)
+            and not any(S in i[3] for S in SKIP_PATHS)
         }
-    for repo_path, manifest_object, manifest_path in manifests:
+    )
+    for module, (repo_path, manifest_object, manifest_path) in manifests.items():
         module_path = os.path.dirname(manifest_path)
 
         try:
@@ -56,7 +81,7 @@ def _find_addons(dir):
         except SyntaxError:
             click.secho("Error Parsing: {}".format(module_path), fg="yellow")
             continue
-        yield os.path.dirname(module_path), os.path.basename(module_path), manifest
+        yield os.path.dirname(module_path), module, manifest
 
 
 def get_graph(dir):
